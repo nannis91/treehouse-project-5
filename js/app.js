3d62d58aa1c51756e0f3aa0099f69e59973421ea
Alexander Lindqvist <alexander@solitudedesign.se>
2024-11-04 15:07:21 +0100

    Add fuction to rotate photos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Initialize baguetteBox.js function for Lightbox 
 baguetteBox.run('.gallery');
+getRandomRotation();
 
 /**
  * Live search in gallery with search box.
@@ -26,3 +27,22 @@ searchInput.addEventListener('input', function() {
         }
     })
   });
+
+
+  /**
+ * Rotates all images with a random angle
+ */
+
+  function getRandomRotation() {
+    // Gets all images and outs them in an array
+    const images = document.querySelectorAll('img');
+    
+    // Takes all items in the array, generates a random number and set number value to a css variable named ---rotation-angle 
+    images.forEach(images => {
+          const randomNumber = Math.random() * (15 - -15 + 1) + -15;
+          images.style.setProperty('--rotation-angle', `${randomNumber}deg`);
+      });
+  
+    }
+  
+  
